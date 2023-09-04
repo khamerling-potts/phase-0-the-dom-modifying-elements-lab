24adf13b10b66efb0fbf05cdf69973c1ea54a211
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-09-04 12:12:59 -0400

    modifying elements lab done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('#main').remove();
+
+const newHeader = document.createElement('h1');
+document.getElementsByTagName('body')[0].appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.textContent = 'Kyra is the champion';
